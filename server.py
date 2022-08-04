b0d053e7843af9cebc737be8130f412e460270f9
bdfd <bdfd2005@gmail.com>
2022-08-04 16:00:54 -0400

    Lesson 2 - Dynamic Website with Jinja2

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 Date         : 2022-08-02 15:04:56
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2022-08-02 17:20:15
+LastEditTime : 2022-08-04 15:52:49
 LastEditors  : BDFD
 Description  : 
 FilePath     : \server.py
@@ -14,11 +14,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  title = "BDFD's Blog"
+  return render_template('index.html', title=title)
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  names = ['John','Mary','Wes','Sally']
+  title = 'About BDFD!'
+  return render_template('about.html', names=names, title=title)
 
 if __name__ == '__main__':
   app.run(debug=True)

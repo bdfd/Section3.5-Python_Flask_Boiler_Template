7b97b483017674f5d3ebc30cbbec88507ad43019
bdfd <bdfd2005@gmail.com>
2022-08-05 15:05:44 -0400

    Lesson 5 Handling Web Form Errors

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 Date         : 2022-08-02 15:04:56
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2022-08-05 12:50:16
+LastEditTime : 2022-08-05 14:35:08
 LastEditors  : BDFD
 Description  : 
 FilePath     : \server.py
@@ -33,8 +33,23 @@ def form():
   first_name = request.form.get('first_name')
   last_name = request.form.get('last_name')
   email = request.form.get('email')
+
+  if not first_name or not last_name or not email:
+    error_statement = 'All Form Fields Required...'
+    return render_template(
+      'fail.html',
+      error_statement=error_statement, 
+      first_name=first_name, 
+      last_name=last_name, 
+      email=email)
+
   subscribes.append(f'{first_name},{last_name}|{email}')
-  return render_template('form.html', subscribes=subscribes)
+  return render_template(
+    'form.html',
+    first_name=first_name,
+    last_name=last_name,
+    email=email,
+    subscribes=subscribes)
 
 if __name__ == '__main__':
   app.run(debug=True)

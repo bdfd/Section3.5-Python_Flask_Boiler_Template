f5c8ec4158c0972e73bc2fde3f50e8fd587ef732
bdfd <bdfd2005@gmail.com>
2022-08-17 15:01:22 -0400

    update function - sent email from subscription page

--- server.py
+++ server.py
@@ -2,16 +2,22 @@
 Date         : 2022-08-02 15:04:56
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2022-08-05 14:35:08
+LastEditTime : 2022-08-17 14:47:50
 LastEditors  : BDFD
 Description  : 
 FilePath     : \server.py
 Copyright (c) 2022 by BDFD, All Rights Reserved. 
 '''
-from requests import request
+# from requests import request
 from flask import Flask, render_template, request
+import smtplib
+import ssl
+from email.message import EmailMessage
+import os
 
 app = Flask(__name__)
+email_sender = 'lejuan.jasiel@gmail.com'
+email_password = 'tebsqtbdgnsfjeqs'
 
 subscribes = []
 
@@ -42,6 +48,26 @@ def form():
       first_name=first_name, 
       last_name=last_name, 
       email=email)
+
+  # Set the subject and body of the email
+  subject = f'Hello, {first_name},{last_name}'
+  body = """
+  hello msg from lejuan
+  """
+
+  em = EmailMessage()
+  em['From'] = email_sender
+  em['To'] = email
+  em['Subject'] = subject
+  em.set_content(body)
+
+  # Add SSL (layer of security)
+  context = ssl.create_default_context()
+
+  # Log in and send the email
+  with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+      smtp.login(email_sender, email_password)
+      smtp.sendmail(email_sender, email, em.as_string())
 
   subscribes.append(f'{first_name},{last_name}|{email}')
   return render_template(
